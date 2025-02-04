a9565887c43da42ed55831ac0d55bfd437078501
Ramnath <ramnath2544@gmail.com>
2025-02-04 11:15:52 +0530

    Make an interactive cart number button slider

--- data/cart.js
+++ data/cart.js
@@ -26,16 +26,20 @@ export function addToCart(productId) {
     }
   });
 
+  const quantitySelector = document.querySelector(
+    `.js-quantity-selector-${productId}`);
+    const quantity = Number(quantitySelector.value);
+
   if (matchingItem) {
-    matchingItem.quantity++;
+    matchingItem.quantity += quantity;
   } else {
     cart.push({
       productId: productId,
-      quantity: 1,
+      quantity: quantity,
       deliveryOptionsId: '1'
     });
   }
-
+ 
   saveToStorage();
 }
 
