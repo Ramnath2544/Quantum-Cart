50d8f6c9006a451c0f33fa7b8ba8a388692da561
Ramnath <ramnath2544@gmail.com>
2025-08-19 10:26:35 +0530

    Fix bug

--- scripts/orders.js
+++ scripts/orders.js
@@ -1,16 +1,16 @@
-import { getProduct, loadProductsFetch } from '../data/products.js';
-import { orders } from '../data/order.js';
-import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
-import { formatCurrency } from './utils/money.js';
-import { addToCart } from '../data/cart.js';
+import { getProduct, loadProductsFetch } from "../data/products.js";
+import { orders } from "../data/order.js";
+import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
+import { formatCurrency } from "./utils/money.js";
+import { addToCart } from "../data/cart.js";
 
 async function loadPage() {
   await loadProductsFetch();
 
-  let ordersHTML = '';
+  let ordersHTML = "";
 
   orders.forEach((order) => {
-    const orderTimeString = dayjs(order.orderTime).format('MMMM D');
+    const orderTimeString = dayjs(order.orderTime).format("MMMM D");
 
     ordersHTML += `
       <div class="order-container">
@@ -38,54 +38,61 @@ async function loadPage() {
   });
 
   function productsListHTML(order) {
-    let productsListHTML = '';
+    let productsListHTML = "";
 
-    order.products.forEach((productDetails) => {
-      const product = getProduct(productDetails.productId);
+    const items = Array.isArray(order?.products) ? order.products : [];
+
+    items.forEach((productDetails) => {
+      const product = getProduct(productDetails.productId) || {};
 
       productsListHTML += `
-        <div class="product-image-container">
-          <img src="${product.image}">
+      <div class="product-image-container">
+        <img src="${product.image || ""}">
+      </div>
+      <div class="product-details">
+        <div class="product-name">
+          ${product.name || "Unknown product"}
         </div>
-        <div class="product-details">
-          <div class="product-name">
-            ${product.name}
-          </div>
-          <div class="product-delivery-date">
-            Arriving on: ${dayjs(productDetails.estimatedDeliveryTime).format('MMMM D')
-        }
-          </div>
-          <div class="product-quantity">
-            Quantity: ${productDetails.quantity}
-          </div>
-          <button class="buy-again-button button-primary js-buy-again"
-           data-product-id="${product.id}">
-            <img class="buy-again-icon" src="images/icons/buy-again.png">
-            <span class="buy-again-message">Buy it again</span>
+        <div class="product-delivery-date">
+          Arriving on: ${
+            productDetails?.estimatedDeliveryTime
+              ? dayjs(productDetails.estimatedDeliveryTime).format("MMMM D")
+              : "N/A"
+          }
+        </div>
+        <div class="product-quantity">
+          Quantity: ${productDetails?.quantity ?? 1}
+        </div>
+        <button class="buy-again-button button-primary js-buy-again"
+         data-product-id="${product.id || ""}">
+          <img class="buy-again-icon" src="images/icons/buy-again.png">
+          <span class="buy-again-message">Buy it again</span>
+        </button>
+      </div>
+      <div class="product-actions">
+        <a href="tracking.html?orderId=${order.id}&productId=${
+        product.id || ""
+      }">
+          <button class="track-package-button button-secondary">
+            Track package
           </button>
-        </div>
-        <div class="product-actions">
-          <a href="tracking.html?orderId=${order.id}&productId=${product.id}">
-            <button class="track-package-button button-secondary">
-              Track package
-            </button>
-          </a>
-        </div>
-      `;
+        </a>
+      </div>
+    `;
     });
 
     return productsListHTML;
   }
 
-  document.querySelector('.js-orders-grid').innerHTML = ordersHTML;
+  document.querySelector(".js-orders-grid").innerHTML = ordersHTML;
 
-  document.querySelectorAll('.js-buy-again').forEach((button) => {
-    button.addEventListener('click', () => {
+  document.querySelectorAll(".js-buy-again").forEach((button) => {
+    button.addEventListener("click", () => {
       addToCart(button.dataset.productId);
 
       // (Optional) display a message that the product was added,
       // then change it back after a second.
-      button.innerHTML = 'Added';
+      button.innerHTML = "Added";
       setTimeout(() => {
         button.innerHTML = `
           <img class="buy-again-icon" src="images/icons/buy-again.png">
