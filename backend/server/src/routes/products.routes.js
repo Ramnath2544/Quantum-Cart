ee20d58b674dcb9d50667ff2f4ca38fb97e169aa
Ramnath <ramnath2544@gmail.com>
2025-08-24 10:52:36 +0530

    Remove all the code of backend server expect static data of product.json

--- backend/server/src/routes/products.routes.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const ctrl = require("../controllers/products.controller");
-
-const router = Router();
-
-router.get("/products", ctrl.listProducts);
-
-module.exports = router;
